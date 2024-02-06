0a1aec782e86904d7446aecc8c49d944ab2e1af1
tales1982 <102876696+tales1982@users.noreply.github.com>
2024-02-06 22:16:08 +0100

    Adcionando rotas

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import BarraLateral from '../../containers/BarraLateral'
+import ListaDeTarefas from '../../containers/ListaDeTarefa'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListaDeTarefas />
+  </>
+)
+
+export default Home
